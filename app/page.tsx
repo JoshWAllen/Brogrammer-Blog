848cd825db8b999d12fec11ee7e24261611c6483
Josh <jwallen883@gmail.com>
2023-05-23 15:38:27 -0400

    Tweaked homepage content and added blog route

--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,15 @@ export default function IndexPage() {
           Brogrammer Blog
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+          A place to share wisdom with the boys about gym, coding, and life.
         </p>
       </div>
       <div className="flex gap-4">
         <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
+          href={siteConfig.links.blog}
           className={buttonVariants({ size: "lg" })}
         >
-          Documentation
+          Enlightenment
         </Link>
         <Link
           target="_blank"
@@ -30,7 +27,7 @@ export default function IndexPage() {
           href={siteConfig.links.github}
           className={buttonVariants({ variant: "outline", size: "lg" })}
         >
-          GitHub
+          Github
         </Link>
       </div>
     </section>
--- config/site.ts
+++ config/site.ts
@@ -1,18 +1,18 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Bro Blog",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "A place to share wisdom with the boys about gym, coding, and life",
   mainNav: [
     {
       title: "Home",
       href: "/",
     },
+    { title: "Blog", href: "/blog" },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    github: "https://github.com/JoshWAllen/Brogrammer-Blog",
+    blog: "/blog",
   },
 }
