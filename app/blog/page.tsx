6dde7c8ac4ea619ed461c075cdfd9052b8f1d4b9
Josh <jwallen883@gmail.com>
2023-06-02 23:03:11 -0400

    blog page links to post gallery and create page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link"
+
+import { buttonVariants } from "@/components/ui/button"
+
 export default function Page() {
-  return <h1>Hello, Blog Page!</h1>
+  return (
+    <>
+      <h1>Hello, Blog Page!</h1>
+      <div className="flex gap-4">
+        <Link href="/blog/posts/" className={buttonVariants({ size: "lg" })}>
+          See Posts
+        </Link>
+        <Link
+          href="/blog/posts/create"
+          className={buttonVariants({ size: "lg" })}
+        >
+          Create Post
+        </Link>
+      </div>
+    </>
+  )
 }
